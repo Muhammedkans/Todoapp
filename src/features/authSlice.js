102a9652c3d2bc6e2d1bf383492919416a1ce10d
Muhammed Kans <kanskabeer@gmail.com>
2025-05-12 10:40:49 +0530

    todo app devloping adding login  and logout feature

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -18,6 +18,72 @@ try{
 
 const loginUser  = createAsyncThunk(
   "auth/login", async(formData , thunkAPI)=>{
-    const response = await axios.post("")
+    try{
+       const response = await axios.post("http://localhost:7777/user/login/todo",formData);
+    }catch(error){
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+   
   }
-)
+);
+
+
+const authSlice = createSlice({
+  name:"auth",
+  initialState:{
+    user:null,
+    token:token,
+    loading:false,
+    error:null
+  },
+  reducers:{
+    logout:(state)=>{
+      state.user = null;
+      state.token =null;
+      localStorage.removeItem("token");
+    }
+  },
+  extraReducers:(builder)=>{
+    
+    builder.addCase(registerUser.pending, (state)=>{
+     state.user = true;
+     state.error = null;
+    })
+
+    builder.addCase(registerUser.fulfilled , (state,action)=>{
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      localStorage.setItem('token',action.payload.token)
+    })
+
+    builder.addCase(registerUser.rejected,(state,action)=>{
+      state.loading =false;
+      state.error = action.payload;
+    })
+
+
+    builder.addCase(loginUser.pending, (state)=>{
+       state.loading = true;
+       state.error =  null;
+    })
+
+    builder.addCase(loginUser.fulfilled, (state, action)=>{
+       state.loading = false;
+       state.user = action.payload.user;
+       state.token = action.payload.token;
+       localStorage.setItem('token',action.payload.token)
+    })
+
+    builder.addCase(loginUser.rejected, (state, action)=>{
+    state.loading = false;
+    state.error = action.payload;
+    })
+  }
+})
+
+
+export const { logout} = authSlice.actions;
+
+export default authSlice.reducer;
+
+
